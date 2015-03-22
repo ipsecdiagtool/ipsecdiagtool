60415cc4f676650c07b7efeb0b97ca0f52c79407
TheoWinter <theo.winter@gmail.com>
2015-03-22 19:54:25 +0100

    Added ability to build a packet for MTU detection

--- mtu/analyze.go
+++ mtu/analyze.go
@@ -1,8 +1,91 @@
 package mtu
 
-import "fmt"
+import (
+	"code.google.com/p/gopacket/examples/util"
+	"net"
+	"log"
+	"code.google.com/p/gopacket/layers"
+	"code.google.com/p/gopacket"
+	"golang.org/x/net/ipv4"
+)
 
 
 func Analyze(){
-	fmt.Println("analyzing mtu...")
+	defer util.Run()()
+	log.Println("Analyzing MTU..")
+	buildPacket("127.0.0.1","127.0.0.1")
+
+
+}
+
+func buildPacket(sourceIP string, destinationIP string){
+	var srcIP = net.ParseIP(sourceIP)
+	var dstIP = net.ParseIP(destinationIP)
+
+	if srcIP == nil || dstIP == nil {
+		log.Println("Invalid IP")
+	}
+
+	//Convert IP to 4bit representation
+	srcIP = srcIP.To4()
+	dstIP = dstIP.To4()
+
+	//IP Layer
+	ip := layers.IPv4{
+		SrcIP:    srcIP,
+		DstIP:    dstIP,
+		Version:  4,
+		TTL:      64,
+		Protocol: layers.IPProtocolTCP,
+	}
+
+	srcPort := layers.TCPPort(666)
+	dstPort := layers.TCPPort(22)
+
+	//TCP Layer
+	tcp := layers.TCP{
+		SrcPort: srcPort,
+		DstPort: dstPort,
+		Window:  1505,
+		Urgent:  0,
+		Seq:     11050,
+		Ack:     0,
+		ACK:     false,
+		SYN:     false,
+		FIN:     false,
+		RST:     false,
+		URG:     false,
+		ECE:     false,
+		CWR:     false,
+		NS:      false,
+		PSH:     false,
+	}
+
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+
+	tcp.SetNetworkLayerForChecksum(&ip)
+
+	ipHeaderBuf := gopacket.NewSerializeBuffer()
+
+	err := ip.SerializeTo(ipHeaderBuf, opts)
+	if err != nil {
+		panic(err)
+	}
+
+	//Set Don't Fragment in Header
+	ipHeader, err := ipv4.ParseHeader(ipHeaderBuf.Bytes())
+	ipHeader.Flags |= ipv4.DontFragment
+	if err != nil {
+		panic(err)
+	}
+
+	tcpPayloadBuf := gopacket.NewSerializeBuffer()
+	payload := gopacket.Payload([]byte("Hello IPSec"))
+	err = gopacket.SerializeLayers(tcpPayloadBuf, opts, &tcp, payload)
+	if err != nil {
+		panic(err)
+	}
 }
