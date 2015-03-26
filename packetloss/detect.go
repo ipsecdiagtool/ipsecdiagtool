e8a1c66b7e2b36df26d148848fced6c95ce62d48
janbalmer <jan.balmer@gmail.com>
2015-03-26 09:15:57 -0700

    Reads Sequenznumbers from IPSec Pakets

--- packetloss/detect.go
+++ packetloss/detect.go
@@ -1,18 +1,81 @@
 package packetloss
 
-import ("code.google.com/p/gopacket"
-	"code.google.com/p/gopacket/pcap"
-	"fmt"
-	"log"
+import (
+        //"bytes"
+        "fmt"
+        "log"
+        "net"
+
+        "code.google.com/p/gopacket"
+        "code.google.com/p/gopacket/layers"
+        "code.google.com/p/gopacket/pcap"
 )
 
 //Detect, for Jan.
 func Detect() {
 	fmt.Println("detecting packetloss ...")
+	
+	//net.Interfaces()
+	iface, err := net.InterfaceByName("any")
+	if err != nil {
+	// error handling
+	}
+	
+	handle, err := pcap.OpenLive("any", 1500, true, 100)
+	if err != nil {
+		panic(err)
+	}
+	
+	stop := make(chan struct{})
+    go readIPSec(handle, iface, stop)
+    defer close(stop)
+    for{fmt.Scanln()}
 }
+
+func readIPSec(handle *pcap.Handle, iface *net.Interface, stop chan struct{}) {
+		fmt.Println("testpunkt1")
+        src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
+        fmt.Println("testpunkt2")
+        in := src.Packets()
+        fmt.Println("testpunkt3")
+        for {
+                var packet gopacket.Packet
+                select {
+                case <-stop:
+                		fmt.Print("*")
+                        return
+                case packet = <-in:
+                		
+                		
+                		
+                		test:=packet.ApplicationLayer()
+                		if test != nil{
+                			fmt.Println(test.Payload())
+                		}
+                		
+                        arpLayer := packet.Layer(layers.LayerTypeIPSecESP)
+                        
+                        if arpLayer == nil {
+                                continue
+                        }
+                        arp := arpLayer.(*layers.IPSecESP)
+                        //if arp.Operation != layers.ARPReply || bytes.Equal([]byte(iface.HardwareAddr), arp.SourceHwAddress) {
+                                // This is a packet I sent.
+                        //        continue
+                        //}
+                        // Note:  we might get some packets here that aren't responses to ones we've sent,
+                        // if for example someone else sends US an ARP request.  Doesn't much matter, though...
+                        // all information is good information :)
+                        //log.Printf("IP %v", net.IP(arp.DstIP))
+                        log.Println(arp.Seq)
+                }
+        }
+}
+
 
 //LiveCapture captures all packets on any interface for an unlimited duration.
 //Packets can be filtered by a BPF filter string. (E.g. tcp port 22)
+/*
 func LiveCapture(bpfFilter string) {
 	log.Println("Waiting for packet")
 	if handle, err := pcap.OpenLive("any", 1500, true, 100); err != nil {
@@ -22,12 +85,15 @@ func LiveCapture(bpfFilter string) {
 	} else {
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 		for packet := range packetSource.Packets() {
-			//Handling packets here
-			log.Println(packet.Dump())
-			log.Println("Received packet with size", packet.Metadata().Length)
+			var packet gopacket.Packet
+			
+
 		}
 	}
 }
+*/
+
+
 
 //ReadPcapFile iterates over all packets in a .pcap-file and counts them.
 //Returns the total number  of packets and outputs the layers of all IPSecESP-Type packets.
