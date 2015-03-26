bbb6b15d4ebf885e1462b6fffce0e2db66f1f9f0
janbalmer <jan.balmer@gmail.com>
2015-03-26 10:23:21 -0700

    Reads Seq number and source/destination from Packets

--- packetloss/detect.go
+++ packetloss/detect.go
@@ -1,27 +1,24 @@
 package packetloss
 
 import (
-        //"bytes"
         "fmt"
         "log"
         "net"
-
         "code.google.com/p/gopacket"
         "code.google.com/p/gopacket/layers"
         "code.google.com/p/gopacket/pcap"
 )
 
-//Detect, for Jan.
 func Detect() {
 	fmt.Println("detecting packetloss ...")
 	
-	//net.Interfaces()
 	iface, err := net.InterfaceByName("any")
 	if err != nil {
 	// error handling
 	}
 	
-	handle, err := pcap.OpenLive("any", 1500, true, 100)
+	//handle, err := pcap.OpenLive("any", 1500, true, 100)
+	handle, err := pcap.OpenOffline("/home/student/TestIpSec_Ping.pcap")
 	if err != nil {
 		panic(err)
 	}
@@ -33,93 +30,15 @@ func Detect() {
 }
 
 func readIPSec(handle *pcap.Handle, iface *net.Interface, stop chan struct{}) {
-		fmt.Println("testpunkt1")
-        src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
-        fmt.Println("testpunkt2")
-        in := src.Packets()
-        fmt.Println("testpunkt3")
-        for {
-                var packet gopacket.Packet
-                select {
-                case <-stop:
-                		fmt.Print("*")
-                        return
-                case packet = <-in:
-                		
-                		
-                		
-                		test:=packet.ApplicationLayer()
-                		if test != nil{
-                			fmt.Println(test.Payload())
-                		}
-                		
-                        arpLayer := packet.Layer(layers.LayerTypeIPSecESP)
-                        
-                        if arpLayer == nil {
-                                continue
-                        }
-                        arp := arpLayer.(*layers.IPSecESP)
-                        //if arp.Operation != layers.ARPReply || bytes.Equal([]byte(iface.HardwareAddr), arp.SourceHwAddress) {
-                                // This is a packet I sent.
-                        //        continue
-                        //}
-                        // Note:  we might get some packets here that aren't responses to ones we've sent,
-                        // if for example someone else sends US an ARP request.  Doesn't much matter, though...
-                        // all information is good information :)
-                        //log.Printf("IP %v", net.IP(arp.DstIP))
-                        log.Println(arp.Seq)
-                }
+        src := gopacket.NewPacketSource(handle, handle.LinkType())
+     
+        for packet := range src.Packets(){
+        	ipsecLayer := packet.Layer(layers.LayerTypeIPSecESP)
+            if ipsecLayer != nil{
+            	netFlow := packet.NetworkLayer().NetworkFlow()
+				src, dst := netFlow.Endpoints()
+                ipsec := ipsecLayer.(*layers.IPSecESP)		
+				log.Println("Source: ",src, "Destination: ",dst,"Seqnumber: ",ipsec.Seq)
+             }
         }
-}
-
-
-//LiveCapture captures all packets on any interface for an unlimited duration.
-//Packets can be filtered by a BPF filter string. (E.g. tcp port 22)
-/*
-func LiveCapture(bpfFilter string) {
-	log.Println("Waiting for packet")
-	if handle, err := pcap.OpenLive("any", 1500, true, 100); err != nil {
-		panic(err)
-	} else if err := handle.SetBPFFilter(bpfFilter); err != nil {
-		panic(err)
-	} else {
-		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-		for packet := range packetSource.Packets() {
-			var packet gopacket.Packet
-			
-
-		}
-	}
-}
-*/
-
-
-
-//ReadPcapFile iterates over all packets in a .pcap-file and counts them.
-//Returns the total number  of packets and outputs the layers of all IPSecESP-Type packets.
-func ReadPcapFile(filepath string) int {
-	var counter = 0
-	if handle, err := pcap.OpenOffline(filepath); err != nil {
-		panic(err)
-	} else {
-		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-		for packet := range packetSource.Packets() {
-			//Handling packets here
-			//fmt.Println(packet.Dump())
-			//fmt.Println(packet.String())
-			var layers []gopacket.Layer
-			layers = append(layers, packet.Layers()...) //Three dots to signify that we're combing two a slices.
-
-			counter++
-
-			//Filtering out only IPSecESP packets.
-			if len(layers) == 3 {
-				if layers[2].LayerType().String() == "IPSecESP" {
-					//Printing the layers each packet has
-					fmt.Println(packet.String())
-				}
-			}
-		}
-	}
-	return counter
 }
