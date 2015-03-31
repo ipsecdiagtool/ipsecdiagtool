8d40d0890b4698705d4b6dd04c7ae36dab5d7a13
TheoWinter <theo.winter@gmail.com>
2015-03-31 15:57:16 +0200

    Sudo su didn't behave as expected, replaced with sudo. Also added docu-download, might be useful.

--- update.sh
+++ update.sh
@@ -3,8 +3,7 @@
 echo "IPSecDiagTool - Updater Utility"
 echo "==============================="
 echo ""
-sudo su
-wget -N http://152.96.56.53:40000/job/IPSecDiagTool%20-%20Application/lastSuccessfulBuild/artifact/bin/ipsecdiagtool
-chmod +x ipsecdiagtool
-echo ""
+sudo wget -N http://152.96.56.53:40000/job/IPSecDiagTool%20-%20Application/lastSuccessfulBuild/artifact/bin/ipsecdiagtool
+sudo chmod +x ipsecdiagtool
+sudo wget -N http://152.96.56.53:40000/job/IPSecDiagTool%20-%20Documentation/lastSuccessfulBuild/artifact/IPSecDiagTool.pdf
 echo "All done. Have a nice day!"
