04b6970e3803cd1432ea1075f7f742acf21b8a7b
TheoWinter <theo.winter@gmail.com>
2015-04-29 13:38:36 +0200

    Prettified build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+#-----------------------------------------------------------#
+#  Title:  build.sh                                         #
+#  URL:    https://github.com/IPSecDiagTool/IPSecDiagTool   #
+#  Author: Jan Balmer, Theo Winter                          #
+#                                                           #
+#  This script can be used to build IPSecDiagTool in a      #
+#  Linux environment.                                       #
+#                                                           #
+#  Dependencies:                                            #
+#   - libpcap-0.8-dev                                       #
+#-----------------------------------------------------------#
+
 echo "Cleaning workspace"
 if [ -d go ]; then
 	rm -rf go1.4.2.linux-amd64.tar.gz
@@ -12,6 +24,7 @@ wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
 tar xf go1.4.2.linux-amd64.tar.gz
 mkdir workspace
 
+export TOP=$(pwd)
 export GOROOT=$(pwd)/go
 export GOPATH=$(pwd)/workspace
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
@@ -19,6 +32,7 @@ export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
 cd workspace
 
 echo "Downloading dependencies"
+#Assuming libpcap-0.8-dev has been installed.
 #sudo apt-get install libpcap-0.8-dev
 go get code.google.com/p/gopacket
 go get code.google.com/p/gopacket/pcap
@@ -34,3 +48,4 @@ cd ipsecdiagtool
 
 echo "Building IPSecDiagTool"
 go build
+mv ipsecdiagtool $TOP/ipsecdiagtool
