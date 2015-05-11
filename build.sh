1e7325b5ebf3f9632320c9d1ec7b8be3404a1174
TheoWinter <theo.winter@gmail.com>
2015-05-11 09:28:34 +0200

    Simplified build-script, removed downloading individual dependencies and manual build process, instead using go get ".." to get all go-dependencies and build in one step.

--- build.sh
+++ build.sh
@@ -19,6 +19,10 @@ if [ -d go ]; then
     rm -rf workspace
 fi
 
+echo "Debug Infos"
+go version
+go env
+
 echo "Setting up Go"
 wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
 tar xf go1.4.2.linux-amd64.tar.gz
@@ -34,17 +38,6 @@ cd workspace
 echo "Downloading dependencies"
 #Assuming libpcap0.8-dev has been installed.
 #sudo apt-get install libpcap-0.8-dev
-go get code.google.com/p/gopacket
-go get code.google.com/p/gopacket/pcap
-go get golang.org/x/net/ipv4
 
-echo "Moving to program directory"
-cd src
-mkdir -p github.com/ipsecdiagtool/
-cd github.com/ipsecdiagtool
-git clone https://github.com/ipsecdiagtool/ipsecdiagtool.git
-cd ipsecdiagtool
-
-echo "Building IPSecDiagTool"
-go build
-mv ipsecdiagtool $TOP/ipsecdiagtool
+echo "Getting and building ipsecdiagtool"
+go get github.com/ipsecdiagtool/ipsecdiagtool
